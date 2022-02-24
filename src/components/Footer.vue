5bb13a148dded700aebeebb5d1f6723ffd7b94d5
Elix Team <elixteam123@gmail.com>
2022-02-24 21:54:34 +0100

    adding new changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,10 +10,10 @@
 
             <div class="footer__services-wrapper">
                 <h3 class="footer__services-heading">Services</h3>
-                <span class="footer__text">Web development</span>
-                <span class="footer__text">E-commerce</span>
-                <span class="footer__text">Maintenance</span>
-                <span class="footer__text">Graphic design</span>
+                <router-link to="/services/web-development" class="footer__text"><span>Web development</span></router-link>
+                <router-link to="/services/e-commerce" class="footer__text"><span>E-commerce</span></router-link>
+                <router-link to="/services/maintenance" class="footer__text"><span>Maintenance</span></router-link>
+                <router-link to="/services/graphic-design" class="footer__text"><span>Graphic design</span></router-link>
             </div>
 
             <div class="footer__resources-wrapper">
@@ -79,6 +79,7 @@
         color: $white;
         display: block;
         padding: .25rem 0;
+        text-decoration: none;
     }
 
     &__newsletter-field {
